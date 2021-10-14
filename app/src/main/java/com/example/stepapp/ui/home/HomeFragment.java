25427d95d7ea23272105ebc303afbfee0c06e49b
Tripparsugo <federico.lagrasta@gmail.com>
2021-10-15 01:13:10 +0200

    WIP

--- app/src/main/java/com/example/stepapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/stepapp/ui/home/HomeFragment.java
@@ -16,7 +16,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.stepapp.R;
@@ -32,9 +31,14 @@ public class HomeFragment extends Fragment {
 
     // Text view and Progress Bar variables
     public TextView stepsCountTextView;
-    public ProgressBar stepsCountProgressBar;
+    public TextView stepsGoalTextView;
 
+    public ProgressBar stepsCountProgressBar;
+    private int stepsGoal = 1000;
     private SensorEventListener listener;
+    private Sensor accSensor;
+    private Sensor stepSensor;
+    private SensorManager sensorManager;
 
     // TODO 1: ACC sensors.
 
@@ -43,10 +47,16 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         // TODO 9: Initialize the TextView variable
+
+        stepsGoalTextView.setText(String.valueOf(stepsGoal));
+        stepsCountTextView = (TextView) root.findViewById(R.id.stepsCount);
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        stepsCountProgressBar = root.findViewById(R.id.progressBar);
 
 
         // TODO 2: Get an instance of the sensor manager.
@@ -56,7 +66,7 @@ public class HomeFragment extends Fragment {
 
         // TODO 11
         // instantiate the StepCounterListener
-        listener = new StepCounterListener();
+        listener = new StepCounterListener(stepsCountTextView, stepsCountProgressBar);
 
         // Toggle group button
         materialButtonToggleGroup = (MaterialButtonToggleGroup) root.findViewById(R.id.toggleButtonGroup);
@@ -69,16 +79,26 @@ public class HomeFragment extends Fragment {
                     Toast.makeText(getContext(), "START", Toast.LENGTH_SHORT).show();
 
                     // TODO 3: Check if the Accelerometer sensor exists
+                    if (accSensor != null) {
+                        sensorManager.registerListener(listener, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    } else {
+                        Toast.makeText(getContext(), R.string.acc_not_available, Toast.LENGTH_SHORT);
+                    }
 
+                    if (stepSensor != null) {
+                        sensorManager.registerListener(listener, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    } else {
+                        Toast.makeText(getContext(), R.string.step_not_available, Toast.LENGTH_SHORT);
+                    }
 
                     // Check if the Step detector sensor exists
-
 
 
                 } else if (group.getCheckedButtonId() == R.id.toggleStop) {
                     //Place code related to Stop button
                     Toast.makeText(getContext(), "STOP", Toast.LENGTH_SHORT).show();
 
+                    sensorManager.unregisterListener(listener);
                     // TODO 4: Unregister the listener
 
                 }
@@ -89,140 +109,164 @@ public class HomeFragment extends Fragment {
 
 
     }
-}
-
-// Sensor event listener
-class StepCounterListener implements SensorEventListener {
-
-    private long lastUpdate = 0;
-
-    // ACC Step counter
-    public static int mACCStepCounter = 0;
-    ArrayList<Integer> mACCSeries = new ArrayList<Integer>();
-    private double accMag = 0;
-    private int lastXPoint = 1;
-    int stepThreshold = 6;
-
-    // Android step detector
-    int mAndroidStepCount = 0;
-
-    // TextView
-    TextView stepsCountTextView;
-
-    //TODO 10
-
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-        switch (event.sensor.getType()) {
-
-            // TODO 5: Get the sensor type
-            case Sensor.TYPE_LINEAR_ACCELERATION:
-
-            // TODO 6: Get sensor's values
-
-
-            //////////////////////////// -- PRINT ACC VALUES -- ////////////////////////////////////
-            // TODO 7: Uncomment the following code
-
-            /*
-                // Timestamp
-                long timeInMillis = System.currentTimeMillis() + (event.timestamp - SystemClock.elapsedRealtimeNanos()) / 1000000;
-
-                // Convert the timestamp to date
-                SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                jdf.setTimeZone(TimeZone.getTimeZone("GMT+2"));
-                String date = jdf.format(timeInMillis);
-
-                // print a value every 1000 ms
-                long curTime = System.currentTimeMillis();
-                if ((curTime - lastUpdate) > 1000) {
-                    lastUpdate = curTime;
-
-                    Log.d("ACC", "X: " + String.valueOf(x) + " Y: " + String.valueOf(y) + " Z: "
-                            + String.valueOf(z) + " t: " + String.valueOf(date));
-
-                }
-
-            */
-
-            ////////////////////////////////////////////////////////////////////////////////////////
-
-            // TODO 8: Compute the ACC magnitude
-
-            //Update the Magnitude series
-
-
-            /// STEP COUNTER ACC ////
-            // Calculate ACC peaks and steps
-
-            peakDetection();
-
-            break;
-
-            // case Step detector
-
-            // Calculate the number of steps
-
-        }
-    }
 
 
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    }
+    // Sensor event listener
+    class StepCounterListener implements SensorEventListener {
 
+        private long lastUpdate = 0;
 
-    private void peakDetection() {
-        int windowSize = 20;
+        // ACC Step counter
+        public static int mACCStepCounter = 0;
+        public static int stepSensorStepCounter = 0;
+        ArrayList<Integer> mACCSeries = new ArrayList<Integer>();
+        private double accMag = 0;
+        private int lastXPoint = 1;
+        int stepThreshold = 6;
+        private final TextView textView;
+        private final ProgressBar stepCountProgressBar;
 
-        /* Peak detection algorithm derived from: A Step Counter Service for Java-Enabled Devices Using a Built-In Accelerometer, Mladenov et al.
-         */
-        int highestValX = mACCSeries.size(); // get the length of the series
-        if (highestValX - lastXPoint < windowSize) { // if the segment is smaller than the processing window skip it
-            return;
-        }
+        // Android step detector
+        int mAndroidStepCount = 0;
 
-        List<Integer> valuesInWindow = mACCSeries.subList(lastXPoint,highestValX);
+        // TextView
+        TextView stepsCountTextView;
 
-        lastXPoint = highestValX;
+        //TODO 10
 
-        int forwardSlope = 0;
-        int downwardSlope = 0;
-
-        List<Integer> dataPointList = new ArrayList<Integer>();
-
-        for (int p =0; p < valuesInWindow.size(); p++){
-            dataPointList.add(valuesInWindow.get(p));
+        StepCounterListener(final TextView textView, final ProgressBar progressBar) {
+            this.stepCountProgressBar = progressBar;
+            this.textView = textView;
         }
 
 
-        for (int i = 0; i < dataPointList.size(); i++) {
-            if (i == 0) {
-            }
-            else if (i < dataPointList.size() - 1) {
-                forwardSlope = dataPointList.get(i + 1) - dataPointList.get(i);
-                downwardSlope = dataPointList.get(i)- dataPointList.get(i - 1);
+        @Override
+        public void onSensorChanged(SensorEvent event) {
 
-                if (forwardSlope < 0 && downwardSlope > 0 && dataPointList.get(i) > stepThreshold ) {
-                    mACCStepCounter += 1;
-                    Log.d("ACC STEPS: ", String.valueOf(mACCStepCounter));
 
-                    //TODO 12: update the text view
+            switch (event.sensor.getType()) {
 
-                }
+
+                // TODO 5: Get the sensor type
+                case Sensor.TYPE_LINEAR_ACCELERATION:
+                    float x = event.values[0];
+                    float y = event.values[1];
+                    float z = event.values[2];
+
+
+                    // TODO 6: Get sensor's values
+
+
+                    //////////////////////////// -- PRINT ACC VALUES -- ////////////////////////////////////
+                    // TODO 7: Uncomment the following code
+
+
+                    // Timestamp
+                    long timeInMillis = System.currentTimeMillis() + (event.timestamp - SystemClock.elapsedRealtimeNanos()) / 1000000;
+
+                    // Convert the timestamp to date
+                    SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    jdf.setTimeZone(TimeZone.getTimeZone("GMT+2"));
+                    String date = jdf.format(timeInMillis);
+
+                    // print a value every 1000 ms
+                    long curTime = System.currentTimeMillis();
+                    if ((curTime - lastUpdate) > 1000) {
+                        lastUpdate = curTime;
+
+                        Log.d("ACCELERATION", "X: " + String.valueOf(x) + " Y: " + String.valueOf(y) + " Z: "
+                                + String.valueOf(z) + " t: " + String.valueOf(date));
+
+                    }
+
+
+                    ////////////////////////////////////////////////////////////////////////////////////////
+
+                    // TODO 8: Compute the ACC magnitude
+
+                    //Update the Magnitude series
+
+
+                    /// STEP COUNTER ACC ////
+                    // Calculate ACC peaks and steps
+                    int acc_magnitude = (int) Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+                    mACCSeries.add(acc_magnitude);
+
+                    peakDetection();
+
+                    break;
+
+                case Sensor.TYPE_STEP_DETECTOR:
+                    countSteps();
+                    break;
+
+                // case Step detector
+
+                // Calculate the number of steps
+
             }
         }
-    }
-
-    // Calculate the number of steps from the step detector
-    private void countSteps(float step) {
 
 
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+        }
+
+
+        private void peakDetection() {
+            int windowSize = 20;
+
+            /* Peak detection algorithm derived from: A Step Counter Service for Java-Enabled Devices Using a Built-In Accelerometer, Mladenov et al.
+             */
+            int highestValX = mACCSeries.size(); // get the length of the series
+            if (highestValX - lastXPoint < windowSize) { // if the segment is smaller than the processing window skip it
+                return;
+            }
+
+            List<Integer> valuesInWindow = mACCSeries.subList(lastXPoint, highestValX);
+
+            lastXPoint = highestValX;
+
+            int forwardSlope = 0;
+            int downwardSlope = 0;
+
+            List<Integer> dataPointList = new ArrayList<Integer>();
+
+            for (int p = 0; p < valuesInWindow.size(); p++) {
+                dataPointList.add(valuesInWindow.get(p));
+            }
+
+
+            for (int i = 0; i < dataPointList.size(); i++) {
+                if (i == 0) {
+                } else if (i < dataPointList.size() - 1) {
+                    forwardSlope = dataPointList.get(i + 1) - dataPointList.get(i);
+                    downwardSlope = dataPointList.get(i) - dataPointList.get(i - 1);
+
+                    Log.d("ACCELERATION SLOPE: ", "F: " + forwardSlope + " D: " + downwardSlope);
+                    if (forwardSlope < 0 && downwardSlope > 0 && dataPointList.get(i) > stepThreshold) {
+                        mACCStepCounter += 1;
+                        Log.d("ACCELERATION STEPS: ", String.valueOf(mACCStepCounter));
+
+                        //TODO 12: update the text view
+
+                    }
+                }
+            }
+        }
+
+        // Calculate the number of steps from the step detector
+        private void countSteps() {
+            stepSensorStepCounter++;
+            textView.setText(String.valueOf(stepSensorStepCounter));
+            Log.d("STEP: ", String.valueOf(stepSensorStepCounter));
+            stepCountProgressBar.setProgress((int) (stepSensorStepCounter/stepGoal));
+        }
     }
 }
+
+
 
